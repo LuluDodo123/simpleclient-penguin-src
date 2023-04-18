32dff565cbc15e859f50fb7b6c7e95f5dda9bc51
LuluDodo <82148090+LuluDodo123@users.noreply.github.com>
2023-04-18 14:16:54 +0200

    Add files via upload

---
+++ src/main/sc/player2023/Debug.java
@@ -0,0 +1,190 @@
+package sc.player2023;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sc.player2023.logic.Logic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Debug {
+    public interface DebugAction {
+        void perform();
+    }
+    private static final Logger log = LoggerFactory.getLogger(Logic.class);
+    /**
+     * Anzahl der Fische welche im bereich des neu platzierten Pinguins sind.
+     */
+    public static final String ANZAHL_FISCHE = "anzahl-fische";
+
+    /**
+     * Anzahl der Felder welche im bereich des neu platzierten Pinguins sind.
+     */
+    public static final String ANZAHL_FELDER = "anzahl-felder";
+
+    /**
+     * Grund warum das Felderfortschreiten gestoppt wurde.
+     */
+    public static final String STOP_GRUND = "stop-grund";
+
+    /**
+     * Felder welche gescanned wurde.
+     */
+    public static final String SCANNED_FIELD = "scanned-field";
+
+    /**
+     * Die Anzahl von Punten welcher ein berechneter Zug hat.
+     */
+    public static final String MOVE_POINTS = "move-points";
+
+    private final HashMap<String, ArrayList<Object>> debug;
+    public Debug(String... debug) {
+        this.debug = new HashMap<>();
+        for (String debugOption:debug) {
+            this.debug.put(debugOption, new ArrayList<>());
+        }
+    }
+
+    public void log(String id) {
+        if (!debug.containsKey(id)) {
+            return;
+        }
+        log.info("DEBUG {}:", id);
+        switch (id) {
+            case ANZAHL_FISCHE -> anzahlFische(getArgs(id));
+            case ANZAHL_FELDER -> anzahlFelder(getArgs(id));
+            case STOP_GRUND -> stopGrund(getArgs(id));
+            case SCANNED_FIELD -> scannedField(getArgs(id));
+            case MOVE_POINTS -> movePoints(getArgs(id));
+        }
+        log.info("");
+    }
+
+    private final int[] fixOrderAnzahl = new int[]{5, 4, 1, 0, 3, 2};
+    private void anzahlFische(Object[] args) {
+        log.info("  {} {} Fische!", args[6], args[7]);
+        displayHex(args, fixOrderAnzahl);
+    }
+
+    private void anzahlFelder(Object[] args) {
+        displayHex(args, fixOrderAnzahl);
+    }
+
+    private void stopGrund(Object[] args) {
+        log.info("  E: Ende des Boards vertikal erreicht.");
+        log.info("  e: Ende des Boards horizontal erreicht.");
+        log.info("  O: Besetztes oder leeres Feld erreicht.");
+        displayHex(args, fixOrderAnzahl);
+    }
+
+    private void scannedField(Object[] args) {
+        log.info("Scanned {}!", args[0]);
+    }
+
+    private void movePoints(Object[] args) { 
+        if ((int) args[1] > (int) args[2]) {
+            log.info("{} {} Punkte! Neue Höchstpunktzahl!", args[0], args[1]);
+        } else {
+            log.info("{} {} Punkte!", args[0], args[1]);
+        }
+    }
+
+    /**
+     * @param sixUnordered Muss genau sechs element beinhalen.
+     * @param order Muss genau sech elemente beinhalten.
+     */
+    private void displayHex(Object[] sixUnordered, int... order) {
+        displayHex(sixUnordered[order[0]], sixUnordered[order[1]], sixUnordered[order[2]], sixUnordered[order[3]], sixUnordered[order[4]], sixUnordered[order[5]]);
+    }
+
+    /**
+     * @param six Muss genau sechs element beinhalen.
+     */
+    private void displayHex(Object[] six) {
+        displayHex(six[0], six[1], six[2], six[3], six[4], six[5]);
+    }
+
+    private void displayHex(Object tl, Object tr, Object l, Object r, Object bl, Object br) {
+        displayHex(tl.toString(), tr.toString(), l.toString(), r.toString(), bl.toString(), br.toString());
+    }
+
+    private void displayHex(String tl, String tr, String l, String r, String bl, String br) {
+        if (tl.length() == 0) {
+            log.info("         {}", tr);
+        } else if (tl.length() == 1) {
+            log.info("      {}  {}", tl, tr);
+        } else if (tl.length() == 2){
+            log.info("     {}  {}", tl, tr);
+        } else {
+            log.info("    {}  {}", tl, tr);
+        }
+        if (l.length() == 0) {
+            log.info("       PP  {}", r);
+        } else if (l.length() == 1) {
+            log.info("    {}  PP  {}", l, r);
+        } else if (l.length() == 2){
+            log.info("   {}  PP  {}", l, r);
+        } else {
+            log.info("  {}  PP  {}", l, r);
+        }
+        if (bl.length() == 0) {
+            log.info("         {}", br);
+        } else if (bl.length() == 1) {
+            log.info("      {}  {}", bl, br);
+        } else if (bl.length() == 2){
+            log.info("     {}  {}", bl, br);
+        } else {
+            log.info("    {}  {}", bl, br);
+        }
+    }
+
+    private Object[] getArgs(String id) {
+        return debug.get(id).toArray();
+    }
+
+    public void setArg(String id, int index, Object arg) {
+        if (!debug.containsKey(id)) {
+            return;
+        }
+        List<Object> args = debug.get(id);
+        if (args.size() == index) {
+            args.add(arg);
+        } else {
+            args.set(index, arg);
+        }
+    }
+
+    public void setArg(String id, int index, Consumer<Object> action) {
+        if (!debug.containsKey(id)) {
+            return;
+        }
+        action.accept(debug.get(id).get(index));
+    }
+
+    public void setArg(String id, Object... args) {
+        if (!debug.containsKey(id)) {
+            return;
+        }
+        debug.put(id, new ArrayList<>(List.of(args)));
+    }
+
+    public void countArg(String id, int index, int amount) {
+        if (!debug.containsKey(id)) {
+            return;
+        }
+        Integer count;
+        try {
+            count = (Integer) debug.get(id).get(index);
+        } catch (IndexOutOfBoundsException e) {
+            count = 0;
+        }
+        List<Object> args = debug.get(id);
+        if (args.size() == index) {
+            args.add(count + amount);
+        } else {
+            args.set(index, count + amount);
+        }
+    }
+}
---
+++ src/main/sc/player2023/logic/Logic.java
@@ -0,0 +1,225 @@
+package sc.player2023.logic;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sc.api.plugins.Coordinates;
+import sc.api.plugins.IGameState;
+import sc.player.IGameHandler;
+import sc.player2023.Debug;
+import sc.plugin2023.Board;
+import sc.plugin2023.Field;
+import sc.plugin2023.GameState;
+import sc.plugin2023.Move;
+import sc.shared.GameResult;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
+
+/**
+ * Das Herz des Clients:
+ * Eine sehr simple Logik, die ihre Zuege zufaellig waehlt,
+ * aber gueltige Zuege macht.
+ * <p>
+ * Ausserdem werden zum Spielverlauf Konsolenausgaben gemacht.
+ */
+public class Logic implements IGameHandler {
+  private static final Logger log = LoggerFactory.getLogger(Logic.class);
+  private static final Debug debug = new Debug(Debug.ANZAHL_FELDER, Debug.ANZAHL_FISCHE, Debug.STOP_GRUND, Debug.SCANNED_FIELD, Debug.MOVE_POINTS);
+
+  /** Aktueller Spielstatus. */
+  private GameState gameState;
+
+  public void onGameOver(@NotNull GameResult data) {
+    log.info("Das Spiel ist beendet, Ergebnis: {}", data);
+  }
+
+  @NotNull
+  @Override
+  public Move calculateMove() {
+    long startTime = System.currentTimeMillis();
+    log.info("Es wurde ein Zug von {} angefordert.", gameState.getCurrentTeam());
+
+    Move move;
+    if(gameState.getTurn() < 8) {
+      move = placePinguin();
+    } else {
+      move = movePinguin();
+    }
+
+    log.info("Sende {} nach {}ms.", move, System.currentTimeMillis() - startTime);
+    return move;
+  }
+  
+  @NotNull
+  public Move movePinguin() {
+    Move doMove;
+    int maxPoints = 0;
+    for (Move move: gameState.getPossibleMoves()) {
+      int points = 0;
+      int[] options = new int(3);
+      foreachReachableFields(move, (field, direction) -> {
+        options[3 - field.getFish()]++;
+      });
+      // Todo: balancing
+      points += 33 * move.getTo().getFish();
+      points += (int) Math.round(Math.sqrt(90 * options[0]));  
+      points += (int) Math.round(Math.sqrt(30 * options[1]));
+      points += (int) Math.round(Math.sqrt(10 * options[2]));
+
+      Debug.setArg(Debug.MOVE_POINTS, move.getTo(), points, maxPoints);
+      Debug.log(Debug.MOVE_POINTS);
+
+      if (points > maxPoints) {
+        maxPoints = points;
+        doMove = move;
+      }
+    }
+    return doMove;
+  }
+
+  // Todo: berechne abschneiden und sicherheit mit ein
+  @NotNull
+  public Move placePinguin() {
+    Move doMove;
+    int maxFische = 0;
+    for (Move move: gameState.getPossibleMoves()) {
+      AtomicInteger fische = new AtomicInteger(0);
+
+      debug.setArg(Debug.ANZAHL_FISCHE, 0, 0, 0, 0, 0, 0);
+      foreachReachableFields(move.getTo(), (field, direction) -> {
+        fische.addAndGet(field.getFish());
+        debug.countArg(Debug.ANZAHL_FISCHE, direction.ordinal(), field.getFish());
+      });
+
+      debug.setArg(Debug.SCANNED_FIELD, 0, move.getTo());
+      debug.log(Debug.SCANNED_FIELD);
+
+      if (fische.get() > maxFische) {
+        maxFische = fische.get();
+        doMove = move;
+        debug.setArg(Debug.ANZAHL_FISCHE, 6, move.getTo());
+        debug.setArg(Debug.ANZAHL_FISCHE, 7, fische.get());
+        debug.log(Debug.ANZAHL_FISCHE);
+        debug.log(Debug.ANZAHL_FELDER);
+        debug.log(Debug.STOP_GRUND);
+      }
+    }
+    return doMove;
+  }
+
+  private enum Direction {
+    RIGHT,
+    LEFT,
+    DOWN_RIGHT,
+    DOWN_LEFT,
+    UP_RIGHT,
+    UP_LEFT
+  }
+
+  public void foreachReachableFields(Coordinates pos, BiConsumer<Field, Direction> action) {
+    debug.setArg(Debug.STOP_GRUND, 'e', 'e', 'E', 'E', 'E', 'E');
+    debug.setArg(Debug.ANZAHL_FELDER, 0, 0, 0, 0, 0, 0);
+    Board board = gameState.getBoard();
+    // Rechts
+    for (int x = pos.getX() + 2; x <= 14; x += 2) {
+      Field field = board.get(x, pos.getY());
+      if (field.isOccupied() || field.isEmpty()) {
+        debug.setArg(Debug.STOP_GRUND, 0, 'O');
+        break;
+      }
+      debug.countArg(Debug.ANZAHL_FELDER, 0, 1);
+      action.accept(field, Direction.RIGHT);
+    }
+
+    // Links
+    for (int x = pos.getX() - 2; x >= 0; x -= 2) {
+      Field field = board.get(x, pos.getY());
+      if (field.isOccupied() || field.isEmpty()) {
+        debug.setArg(Debug.STOP_GRUND, 1, 'O');
+        break;
+      }
+      debug.countArg(Debug.ANZAHL_FELDER, 1, 1);
+      action.accept(field, Direction.LEFT);
+    }
+
+    // Runter-R
+    int x = pos.getX();
+    for (int y = pos.getY() + 1; y <= 7; y++) {
+      x++;
+      if (x > 14) {
+        debug.setArg(Debug.STOP_GRUND, 2, 'e');
+        break;
+      }
+      Field field = board.get(x, y);
+      if (field.isOccupied() || field.isEmpty()) {
+        debug.setArg(Debug.STOP_GRUND, 2, 'O');
+        break;
+      }
+      debug.countArg(Debug.ANZAHL_FELDER, 2, 1);
+      action.accept(field, Direction.DOWN_RIGHT);
+    }
+
+    // Runter-L
+    x = pos.getX();
+    for (int y = pos.getY() + 1; y <= 7; y++) {
+      x--;
+      if (x < 0) {
+        debug.setArg(Debug.STOP_GRUND, 3, 'e');
+        break;
+      }
+      Field field = board.get(x, y);
+      if (field.isOccupied() || field.isEmpty()) {
+        debug.setArg(Debug.STOP_GRUND, 3, 'O');
+        break;
+      }
+      debug.countArg(Debug.ANZAHL_FELDER, 3, 1);
+      action.accept(field, Direction.DOWN_LEFT);
+    }
+
+    // Hoch-R
+    x = pos.getX();
+    for (int y = pos.getY() - 1; y >= 0; y--) {
+      x++;
+      if (x > 14) {
+        debug.setArg(Debug.STOP_GRUND, 4, 'e');
+        break;
+      }
+      Field field = board.get(x, y);
+      if (field.isOccupied() || field.isEmpty()) {
+        debug.setArg(Debug.STOP_GRUND, 4, 'O');
+        break;
+      }
+      debug.countArg(Debug.ANZAHL_FELDER, 4, 1);
+      action.accept(field, Direction.UP_RIGHT);
+    }
+
+    // Hoch-L
+    x = pos.getX();
+    for (int y = pos.getY() - 1; y >= 0; y--) {
+      x--;
+      if (x < 0) {
+        debug.setArg(Debug.STOP_GRUND, 5, 'e');
+        break;
+      }
+      Field field = board.get(x, y);
+      if (field.isOccupied() || field.isEmpty()) {
+        debug.setArg(Debug.STOP_GRUND, 5, 'O');
+        break;
+      }
+      debug.countArg(Debug.ANZAHL_FELDER, 5, 1);
+      action.accept(field, Direction.UP_LEFT);
+    }
+  }
+
+  @Override
+  public void onUpdate(IGameState gameState) {
+    this.gameState = (GameState) gameState;
+    log.info("Zug: {} Dran: {}", gameState.getTurn(), gameState.getCurrentTeam());
+  }
+
+  @Override
+  public void onError(@NotNull String error) {
+    log.warn("Fehler: {}", error);
+  }
+}
